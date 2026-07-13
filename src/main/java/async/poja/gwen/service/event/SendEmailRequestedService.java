1a1ce3b7b08555335d4f28cbece500279e207887
Gwynbleidd <82315666+Gwynbleidd344@users.noreply.github.com>
2026-07-13 07:37:55 +0300

    Fixed the SendEmailRequestedService class to use temporary link (#3)
    * Fixed the SendEmailRequestedService class to use temporary link
    
    * Fix: format

--- src/main/java/async/poja/gwen/service/event/SendEmailRequestedService.java
+++ src/main/java/async/poja/gwen/service/event/SendEmailRequestedService.java
@@ -1,11 +1,14 @@
 package async.poja.gwen.service.event;
 
 import async.poja.gwen.endpoint.event.model.SendEmailRequested;
+import async.poja.gwen.file.bucket.BucketComponent;
 import async.poja.gwen.mail.Email;
 import async.poja.gwen.mail.Mailer;
 import async.poja.gwen.mail.PdfGenerator;
 import jakarta.mail.internet.InternetAddress;
+import java.time.Duration;
 import java.util.List;
+import java.util.UUID;
 import java.util.function.Consumer;
 import lombok.AllArgsConstructor;
 import lombok.SneakyThrows;
@@ -17,6 +20,7 @@ public class SendEmailRequestedService implements Consumer<SendEmailRequested> {
 
   private final Mailer mailer;
   private final PdfGenerator pdfGenerator;
+  private final BucketComponent bucketComponent;
 
   @SneakyThrows
   @Override
@@ -24,7 +28,18 @@ public class SendEmailRequestedService implements Consumer<SendEmailRequested> {
     var pdfHtmlContent = "<html><body><p>PDF file succesfully created</p></body></html>";
     var pdfFile = pdfGenerator.apply(pdfHtmlContent);
 
-    var emailHtmlBody = "Cher " + event.getNom() + ",";
+    var bucketKey = "documents/" + UUID.randomUUID() + ".pdf";
+    bucketComponent.upload(pdfFile, bucketKey);
+    var presignedUrl = bucketComponent.presign(bucketKey, Duration.ofHours(24));
+
+    var emailHtmlBody =
+        "Cher "
+            + event.getNom()
+            + ",<br><br>"
+            + "Voici le lien pour télécharger votre document (valable 24h) : <br>"
+            + "<a href=\""
+            + presignedUrl
+            + "\">Télécharger le PDF</a>";
 
     var email =
         new Email(
@@ -33,7 +48,7 @@ public class SendEmailRequestedService implements Consumer<SendEmailRequested> {
             List.of(),
             "Votre document est prêt",
             emailHtmlBody,
-            List.of(pdfFile));
+            List.of());
 
     mailer.accept(email);
   }
